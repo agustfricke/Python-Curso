163183caef3049b506df01fb357cae21806aaef1
Agust <agustfricke@protonmail.com>
2022-12-01 17:52:14 -0300

    sintaxis

--- 01-sintaxis.py
+++ 01-sintaxis.py
@@ -1,6 +1,33 @@
-if 5 > 2:
-    print('5 es mayor a 2')
-    print('5 es mayor a 2') 
+# Sangria en Python
+if 10 > 5:
+   print('10 es mayor que 5')
 
-# Comentario
+# Error
+if 10 > 5:
+print('10 es mayor que 5')
+
+# 4 espacios
+if 10 > 5:
+   print('10 es mayor que 5')
+ 
+# 1 espacio
+if 10 > 5:
+print('10 es mayor que 5')
+
+# Error
+if 10 > 5:
+print('10 es mayor que 5')
+   print('10 es mayor que 5')
+
+
+# Variables en Python
+nombre = 'Agustin'
+edad = 22
+
+# Comenatarios en Python
+
+# Esto es un comentario
+if 10 > 5:
+   print('10 es mayor que 5')
+# Esto también es un comentario
 
