d5c8bd5c97bbf44a8fb246774497e8a6d0fb19bd
Agust <agustfricke@protonmail.com>
2022-12-04 00:41:23 -0300

    committtt

---
+++ test.py
@@ -0,0 +1,25 @@
+persona = {
+   'nombre': 'Agustin',
+   'apellido': 'Fricke',
+   'edad': 22,
+   'es_admin': True
+}
+
+copiaPersona = dict(persona)
+
+
+users = {
+  'user1' : {
+    'user_name' : 'Random',
+    'email' : 'random@random.com'
+  },
+  'user2' : {
+    'user_name' : 'Usuario',
+    'email' : 'usuario@gmail.com'
+  },
+  'user3' : {
+    'user_name' : 'admin',
+    'email' : 'admin@admin.com'
+  }
+}
+print(users)
